fad0dad00ff6f47cd01d251645a2003d08686ab8
brandinio <brandinio@outlook.com>
2020-08-02 17:32:56 +0100

    Renamed Air Conditioner to Tower Fan

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (homebridge) => {
     // eslint-disable-next-line global-require
-    const IgenixAirConditioner = require('./lib/IgenixAirConditioner')(homebridge);
-    homebridge.registerAccessory('homebridge-igenix-air-conditioner', 'IgenixAirConditioner', IgenixAirConditioner);
+    const IgenixTowerFan = require('./lib/IgenixTowerFan')(homebridge);
+    homebridge.registerAccessory('homebridge-igenix-tower-fan', 'IgenixTowerFan', IgenixTowerFan);
 };
